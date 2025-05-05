d3f3ceb7b8b4a12b1f076bd6cf3d9c22727b613e
Brighton <BrightonDube@users.noreply.github.com>
2025-05-06 00:14:00 +0200

    `Refactor addProductToCart function to handle non-array local storage values`

--- src/js/product.js
+++ src/js/product.js
@@ -4,7 +4,10 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  const cart = getLocalStorage("so-cart") || [];
+  let cart = getLocalStorage("so-cart");
+  if (!Array.isArray(cart)) {
+    cart = [];
+  }
   cart.push(product);
   setLocalStorage("so-cart", cart);
 }
