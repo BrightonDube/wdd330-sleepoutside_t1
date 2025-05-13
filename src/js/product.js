4738c4bce2112533dd579c8358bed9b45a130937
DaveFont19 <dfontes@byupathway.edu>
2025-05-13 08:59:10 -0600

    team unresolve activity

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,13 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
+const productId = getParam('product');
+
+console.log(dataSource.findProductById(productId));
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const product = urlParams.get('product');
 
 function addProductToCart(product) {
   setLocalStorage("so-cart", product);
